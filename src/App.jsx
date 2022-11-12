f0ea9a4cf452fd7697f128fa38b4659a16ac36f9
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2022-11-12 16:37:49 -0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="App-Nav">
+          <div className="App-Nav-Element">
+            &lt;name logo thing&gt;
+          </div>
+          <div className="App-Nav-Element" style={{width: "30%"}}>
+            search goes here
+          </div>
+          <div className="App-Nav-Element" style={{width: "40%"}}>
+            more stuff
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
