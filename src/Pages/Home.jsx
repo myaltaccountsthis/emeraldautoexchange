633dcfa8d31ef4fb0374dd825cc92be2c1fa6d82
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2022-11-16 21:50:48 -0600

    Start on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,19 @@ class Home extends React.Component {
         return (
             <div className="Page">
                 <div className="App-Header">Welcome to Emerald Auto Exchange</div>
+                <br />
+                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
+                    
+                </div>
+                <div className="Info-Row">
+
+                </div>
+                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
+
+                </div>
+                <div className="Info-Row">
+
+                </div>
             </div>
         );
     }
