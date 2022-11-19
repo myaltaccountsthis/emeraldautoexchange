2283e0589d1085706a0602ecc0c177eb73bfe4b1
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2022-11-19 13:14:30 -0600

    Getting ready for home image gallery

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,9 +6,17 @@ class Home extends React.Component {
             <div className="Page">
                 <div className="App-Header">Welcome to Emerald Auto Exchange</div>
                 <br />
-                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
-                    
+                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)", padding: "50px 0px 0px 0px" }}>
+                    <div style={{ height: "400px", padding: "0px 200px" }}>
+                        <img src="emerald.png" alt="Car" style={{ objectFit: "contain", height: "300px", margin: "50px 0px" }}></img>
+                    </div>
+                    <div>
+                        <button className="Home-Image-Button"></button>
+                        <button className="Home-Image-Button"></button>
+                        <button className="Home-Image-Button"></button>
+                    </div>
                 </div>
+                <div style={{ fontSize: 40, height: "60px", padding: "20px" }}>About Us</div>
                 <div className="Info-Row">
 
                 </div>
