7180614d32c6afffbd038b31aac63d9bf35349ad
Metahuman Flash <sahasradsai@gmail.com>
2022-11-20 15:02:38 -0600

    Initialize Backend

---
+++ backend/src/test/java/com/emeraldautoexchange/eaebackend/EaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emeraldautoexchange.eaebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
