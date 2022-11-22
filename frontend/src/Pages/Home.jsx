7ac00ac84b2a221178adcb2145864ec2a929f882
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2022-11-21 18:21:44 -0600

    Image and gallery system

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,48 +2,68 @@ import React from 'react';
 import './Home.css';
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="Page">
-                <div className="App-Header-Row">
-                    <div className="App-Header">Welcome to Emerald Auto Exchange</div><br />
-                    <div style={{ fontSize: "24px" }}>Finer Cars for Finer People</div>
-                </div>
-                <br />
-                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
-                    <div className="Home-Image-Row">
-                        <button className="Home-Image-Arrow">
-                            <img src="arrowL.png" alt="Left"></img>
-                        </button>
-                        <img src="emerald.png" alt="Car"></img>
-                        <button className="Home-Image-Arrow">
-                            <img src="arrowR.png" alt="Right"></img>
-                        </button>
-                    </div>
-                    <div>
-                        <button className="Home-Image-Button"></button>
-                        <button className="Home-Image-Button"></button>
-                        <button className="Home-Image-Button"></button>
-                    </div>
-                </div>
-                <div className="Info-Row">
-                    <div style={{ fontSize: 40, padding: "20px" }}>About Us</div>
-                    <div className="Home-About-Text">
-                        Emerald Auto Exchange was created to give customers used cars for the price they are worth.
-                        We strive to keep prices fair for our many cars of various conditions.
-                        Our wide selection of used cars will ensure that you can find the right car for the right price.
-                    </div>
-                    <br />
-                </div>
-                <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImage: 0
+    };
+    this.images = ["emerald.png", "emeraldicon.png", "logo.png"];
+  }
 
-                </div>
-                <div className="Info-Row">
+  componentDidMount() {
+    this.images.forEach(image => new Image().src = image);
+  }
 
-                </div>
-            </div>
-        );
-    }
+  setImage(i) {
+    this.setState({ currentImage: i});
+  }
+
+  changeImage(delta) {
+    this.setImage((this.state.currentImage + 3 + delta) % 3);
+  }
+
+  render() {
+    return (
+      <div className="Page">
+        <div className="App-Header-Row">
+          <div className="App-Header">Welcome to Emerald Auto Exchange</div><br />
+          <div style={{ fontSize: "24px" }}>Finer Cars for Finer People</div>
+        </div>
+        <br />
+        <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
+          <div className="Home-Image-Row">
+            <button className="Home-Image-Arrow" onClick={() => this.changeImage(-1)}>
+              <img src="arrowL.png" alt="Left"></img>
+            </button>
+            <img src={this.images[this.state.currentImage]} alt="Car"></img>
+            <button className="Home-Image-Arrow" onClick={() => this.changeImage(1)}>
+              <img src="arrowR.png" alt="Right"></img>
+            </button>
+          </div>
+          <div>
+            {
+              this.images.map((_, index) => <button className={"Home-Image-Button " + (this.state.currentImage === index ? "Home-Image-Button-Active" : "")} key={index} onClick={() => this.setImage(index)} />)
+            }
+          </div>
+        </div>
+        <div className="Info-Row">
+          <div style={{ fontSize: 40, padding: "20px" }}>About Us</div>
+          <div className="Home-About-Text">
+            Emerald Auto Exchange was created to give customers used cars for the price they are worth.
+            We strive to keep prices fair for our many cars of various conditions.
+            Our wide selection of used cars will ensure that you can find the right car for the right price.
+          </div>
+          <br />
+        </div>
+        <div className="Info-Row" style={{ backgroundColor: "rgb(192, 248, 215)" }}>
+
+        </div>
+        <div className="Info-Row">
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
