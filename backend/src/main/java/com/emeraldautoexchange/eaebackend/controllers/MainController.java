e0cd83230e06158c722b300fdac65e1a6b6e2b6e
MetaFlash <sahasradsai@gmail.com>
2022-12-03 09:55:16 -0600

    Update MainController.java

--- backend/src/main/java/com/emeraldautoexchange/eaebackend/controllers/MainController.java
+++ backend/src/main/java/com/emeraldautoexchange/eaebackend/controllers/MainController.java
@@ -1,13 +1,26 @@
 package com.emeraldautoexchange.eaebackend.controllers;
 
+import com.emeraldautoexchange.eaebackend.Vehicle;
+import com.emeraldautoexchange.eaebackend.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MainController {
+    @Autowired
+    VehicleRepository repo;
+
     @GetMapping("/add/{num1}/{num2}")
     int add(@PathVariable int num1, @PathVariable int num2) {
         return num1 + num2;
+    }
+
+    @GetMapping("/inventory")
+    List<Vehicle> getInventory() {
+        return repo.findAllByOrderByIdAsc();
     }
 }
