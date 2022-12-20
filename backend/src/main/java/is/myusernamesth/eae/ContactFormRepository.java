dce229418eaf4e4af494fc515644d2ad9313d6d2
MetaFlash <sahasradsai@gmail.com>
2022-12-19 18:17:11 -0600

    Small Fix
    Contact form wipes need to be transactional to execute alter queries.

--- backend/src/main/java/is/myusernamesth/eae/ContactFormRepository.java
+++ backend/src/main/java/is/myusernamesth/eae/ContactFormRepository.java
@@ -3,11 +3,12 @@ package is.myusernamesth.eae;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ContactFormRepository extends JpaRepository<ContactForm, Integer> {
 
     @Modifying
+    @Transactional
     @Query(value = "TRUNCATE contact_forms RESTART IDENTITY; ALTER SEQUENCE hibernate_sequence RESTART WITH 1;", nativeQuery = true)
     void wipe();
 }
